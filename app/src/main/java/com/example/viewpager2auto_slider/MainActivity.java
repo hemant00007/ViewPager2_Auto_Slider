a876f6f4a93e3cc7016de65a23b03343d6c38fdf
hemant <hemantkumar692@gmail.com>
2021-01-04 22:03:55 +0530

    second commit auto scroll

--- app/src/main/java/com/example/viewpager2auto_slider/MainActivity.java
+++ app/src/main/java/com/example/viewpager2auto_slider/MainActivity.java
@@ -35,19 +35,19 @@ public class MainActivity extends AppCompatActivity {
         viewPager2.setClipToPadding(false);
         viewPager2.setClipChildren(false);
         viewPager2.setOffscreenPageLimit(3);
-        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
-
-        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
-        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
-        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
-            @Override
-            public void transformPage(@NonNull View page, float position) {
-                float r = 1 - Math.abs(position);
-                page.setScaleY(0.85f + r + 0.15f);
-
-            }
-        });
-        viewPager2.setPageTransformer(compositePageTransformer);
+//        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+//
+//        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+//        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+//        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+//            @Override
+//            public void transformPage(@NonNull View page, float position) {
+//                float r = 1 - Math.abs(position);
+//                page.setScaleY(0.85f + r + 0.15f);
+//
+//            }
+//        });
+//        viewPager2.setPageTransformer(compositePageTransformer);
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
